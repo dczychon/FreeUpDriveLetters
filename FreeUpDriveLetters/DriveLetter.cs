a2df5538864e60dcfca4af685d0e5d8c0deaad47
Dominik C <d.czychon@outlook.com>
2017-08-11 17:48:50 +0200

    Projektdateien hinzufügen.

---
+++ FreeUpDriveLetters/DriveLetter.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.IO;
+
+namespace FreeUpDriveLetters
+{
+    /// <summary>
+    /// Stellt einen Laufwerksbuchstaben dar
+    /// </summary>
+    class DriveLetter
+    {
+        private const string MountedDevicesRegKey = @"SYSTEM\MountedDevices";
+
+        /// <summary>
+        /// Systemkritische Laufwerksbuchstaben
+        /// </summary>
+        private static readonly string[] SystemCriticalLetters = { "c" };
+
+        /// <summary>
+        /// Laufwerksbuchstabe
+        /// </summary>
+        public char Letter { get; set; }
+
+        /// <summary>
+        /// Gibt an, ob dieser Laufwerksbuschstabe entfernt werden kann
+        /// </summary>
+        public bool CanBeRemoved { get; private set; }
+
+        /// <summary>
+        /// Wenn dieser Wert auf True gesetzt ist, wird dieser Laufwerksbuschstabe beim Clean aus der Registry gelöscht
+        /// </summary>
+        public bool MarkedForRemoval { get; set; } = false;
+
+        /// <summary>
+        /// Gibt den Namen, der Value in der Registry, für diesen <see cref="DriveLetter"/> zurück
+        /// </summary>
+        public string ExpectedRegistryValueName { get { return string.Format(@"\DosDevices\{0}:", Letter.ToString().ToUpper()); } }
+
+        /// <summary>
+        /// Ob der Laufwerksbuchstabe einen Eintrag in der Registry besitzt
+        /// </summary>
+        public bool Exists
+        {
+            get
+            {
+                try
+                {
+                    RegistryKey MountedDevices = GetMountedDevicesRegistryKey(false);
+                    var content = MountedDevices.GetValue(ExpectedRegistryValueName);
+
+                    if (content == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gibt an, ob ein Gerät mit diesem Laufwerksbuchstaben gerade angeschlossen ist
+        /// </summary>
+        public bool CurrentlyMounted
+        {
+            get
+            {
+                return DriveLetterIsCurrentlyMounted(Letter);
+            }
+        }
+
+        /// <summary>
+        /// Gibt das Volume Label für diesen <see cref="DriveLetter"/> zurück
+        /// </summary>
+        public string DriveNameOrNull
+        {
+            get
+            {
+                try
+                {
+                    return GetDriveInfoForMountedDevice(this).VolumeLabel;
+                }
+                catch (IOException)
+                {
+                    //Gerät mit diesem Laufwerksbuchstaben ist nicht gemounted
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Erstellt einen neuen <see cref="DriveLetter"/> mit dem übergbenen Buchstaben
+        /// </summary>
+        /// <param name="Drive">Laufwerksbuchstabe</param>
+        public DriveLetter(char Drive)
+        {
+            Letter = Drive;
+
+            if (IsProtectedLetter(Drive) || DriveLetterIsCurrentlyMounted(Drive))
+            {
+                //Der übergebene Laufwerksbuchstabe ist gearde von einem Gerät in verwendung oder Systemkritisch
+                CanBeRemoved = false;
+            }
+            else
+            {
+                CanBeRemoved = true;
+            }
+        }
+
+        /// <summary>
+        /// Erstellt einen neuen <see cref="DriveLetter"/> und überschreibt die CanBeRemoved Eigenschaft
+        /// </summary>
+        /// <param name="Drive">Laufwerksbuschstabe</param>
+        /// <param name="AlwaysAllowRemove">Ob dieser Laufwerksbuschstabe aus der Registry entfernt werden darf (VORSICHT!!)</param>
+        public DriveLetter(char Drive, bool AlwaysAllowRemove) : this(Drive)
+        {
+            if (AlwaysAllowRemove)
+            {
+                if (CanBeRemoved == false)
+                {
+                    CanBeRemoved = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Löscht den Laufwerksbuchstaben aus der Registry
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Wenn ein entfernen nicht erlaubt ist</exception>
+        public void Delete()
+        {
+            if (CanBeRemoved)
+            {
+                DeleteFromRegistry(this, true);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Der Laufwerksbuchstabe {Letter} kann nicht entfernt werden, weil er entweder gerade von einem angeschlossenen Gerät genutzt wird oder Systemkritisch ist");
+            }
+        }
+
+        /// <summary>
+        /// Gibt <see cref="DriveInfo"/> des Gerätes mit diesem Laufwerksbuchstaben zurück
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="IOException">Wenn der Laufwerksbuchstabe momentan nicht gemounted ist</exception>
+        public DriveInfo GetDriveInfo()
+        {
+            return GetDriveInfoForMountedDevice(this);
+        }
+
+
+        #region "Statisches Zeug"
+        /// <summary>
+        /// Prüft ob der Laufwerksbuchstabe ein Systemkritischer ist
+        /// </summary>
+        /// <param name="Letter">Zu prüfender Buchstabe</param>
+        /// <returns></returns>
+        private static bool IsProtectedLetter(char Letter)
+        {
+            foreach (string pl in SystemCriticalLetters)
+            {
+                if (Letter.ToString().ToLower().Contains(pl))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Prüft ob der Laufwerksbuchstabe ein Systemkritischer ist
+        /// </summary>
+        /// <param name="Letter">Zu prüfender Buchstabe</param>
+        /// <returns></returns>
+        public static bool IsProtectedLetter(DriveLetter Letter)
+        {
+            return IsProtectedLetter(Letter.Letter);
+        }
+
+        /// <summary>
+        /// Gibt die Reservierten Laufwerksbuchstaben aus der Registry zurück
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static IEnumerable<DriveLetter> GetReservedFromRegistry(bool AlwaysAllowRemove = false)
+        {
+            List<DriveLetter> ReservedLetters = new List<DriveLetter>();
+
+            //Öffnet den Key mit den reservierten Laufwerksbuchstaben
+            RegistryKey MountedDevicesKey = GetMountedDevicesRegistryKey(false);
+
+            //Loopt durch jede Value in dem RegistryKey
+            foreach (string MountedDevice in MountedDevicesKey.GetValueNames())
+            {
+                if (IsValueNameReservedLetter(MountedDevice))
+                {
+                    //Die aktuelle Value ist ein Laufwerksbuchstabe
+                    ReservedLetters.Add(new DriveLetter(ExtractDriveLetterFromValueName(MountedDevice), AlwaysAllowRemove));
+                }
+            }
+
+            return ReservedLetters;
+        }
+
+        /// <summary>
+        /// Prüft ob der übergebene Value Name ein Eintrag für einen Reservierten Laufwerksbuchstaben ist
+        /// </summary>
+        /// <param name="ValueName">Zu prüfender Value Name</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Wenn der Value Name null ist</exception>
+        private static bool IsValueNameReservedLetter(string ValueName)
+        {
+            if (string.IsNullOrEmpty(ValueName))
+            {
+                throw new ArgumentNullException("ValueName");
+            }
+
+            return ValueName.ToLower().Contains(@"\dosdevices\");
+        }
+
+        /// <summary>
+        /// Extrahiert aus einem Value Namen einen Drive Letter
+        /// </summary>
+        /// <param name="ValueName">Name aus dem extrahiert werden soll</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Wenn der Value Name nicht für einen Reservierten Laufwerksbuchstaben steht</exception>
+        /// <exception cref="ArgumentNullException">Wenn der Value Name null ist</exception>
+        private static char ExtractDriveLetterFromValueName(string ValueName)
+        {
+            if (string.IsNullOrEmpty(ValueName))
+            {
+                throw new ArgumentNullException("ValueName");
+            }
+
+            if (IsValueNameReservedLetter(ValueName) == false)
+            {
+                throw new ArgumentException("ValueName", $"Der übergebene Value Name '{ValueName}' steht nicht für einen reservierten Laufwerksbuchstaben");
+            }
+            
+            return Convert.ToChar(ValueName.Substring(12, 1));
+        }
+
+        /// <summary>
+        /// Löscht den übergebenen RegistryKey aus der Registry
+        /// </summary>
+        /// <param name="Letter">Zu löschender Laufwerksbuchstabe</param>
+        /// <param name="force">Wenn true, wird er entfernt, auch wenn er gerade in Benutzung ist</param>
+        /// <exception cref="InvalidOperationException">Wenn der Laufwerksbuchstabe gearde von einem Gerät verwendet wird</exception>
+        public static void DeleteFromRegistry(DriveLetter Letter, bool force = false)
+        {
+            if (force == false)
+            {
+                if (DriveLetterIsCurrentlyMounted(Letter.Letter))
+                {
+                    throw new InvalidOperationException($"Der Laufwerksbuchstabe kann nicht aus der Registry entfernt werden, weil er gerade von einem Gerät verwendet wird");
+                }
+            }
+
+            RegistryKey MountedDevicesKey = GetMountedDevicesRegistryKey(true);
+            MountedDevicesKey.DeleteValue(Letter.ExpectedRegistryValueName);
+        }
+
+        /// <summary>
+        /// Gibt den geöffneten MountedDevices Registry Key zurück
+        /// </summary>
+        /// <returns></returns>
+        private static RegistryKey GetMountedDevicesRegistryKey(bool Writable)
+        {
+            return RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(MountedDevicesRegKey, Writable);
+        }
+
+        /// <summary>
+        /// Gibt an, ob der übergebene Laufwerksbuchstabe einem momentan angeschlossenen Gerät zugeordnet ist
+        /// </summary>
+        /// <param name="Letter">Zu prüfender Laufwerksbuchstabe</param>
+        /// <returns></returns>
+        private static bool DriveLetterIsCurrentlyMounted(char Letter)
+        {
+            foreach (DriveInfo MountedDrive in DriveInfo.GetDrives())
+            {
+                if (MountedDrive.Name.Substring(0, 1).ToLower() == Letter.ToString().ToLower())
+                {
+                    //Der Laufwerksbuchstabe ist momentant in use
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gibt zurück wieviele inder der Liste aus der Registry entfernt werden könnten
+        /// </summary>
+        /// <param name="Letters"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">Letters ist null oder leer</exception>
+        public static int AvailableForRemove(IEnumerable<DriveLetter> Letters)
+        {
+            if (Letters == null || Letters.Count() <= 0)
+            {
+                throw new ArgumentNullException("Letters");
+            }
+
+            int counter = 0;
+
+            foreach (DriveLetter l in Letters)
+            {
+                if (l.CanBeRemoved)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        /// <summary>
+        /// Gibt <see cref="DriveInfo"/> für den übergebnen Laufwerksbuchstaben zurück
+        /// </summary>
+        /// <param name="Letter">Laufwerksbuchstabe</param>
+        /// <returns></returns>
+        /// <exception cref="IOException">Gerät mit dem übergebnenen Laufwerksbuchstaben ist nicht gemounted</exception>
+        public static DriveInfo GetDriveInfoForMountedDevice(char Letter)
+        {
+            foreach (DriveInfo MountedDrive in DriveInfo.GetDrives())
+            {
+                if (MountedDrive.Name.Substring(0, 1).ToLower() == Letter.ToString().ToLower())
+                {
+                    return MountedDrive;
+                }
+            }
+
+            //Wenn momentan kein Gerät mit dem Laufwerksbuchstaben angeschlossen ist
+            throw new IOException($"Es ist kein Gerät mit dem Laufwerksbuchstaben {Letter} gemounted");
+        }
+
+        /// <summary>
+        /// Gibt <see cref="DriveInfo"/> für den übergebnen Laufwerksbuchstaben zurück
+        /// </summary>
+        /// <param name="Letter">DriveLetter Information</param>
+        /// <returns></returns>
+        /// <exception cref="IOException">Gerät mit dem übergebnenen Laufwerksbuchstaben ist nicht gemounted</exception>
+        public static DriveInfo GetDriveInfoForMountedDevice(DriveLetter Letter)
+        {
+            return GetDriveInfoForMountedDevice(Letter.Letter);
+        }
+        #endregion
+    }
+}
---
+++ FreeUpDriveLetters/MainWindow.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace FreeUpDriveLetters
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<DriveLetter> SystemDriveLetters;
+        bool AlwaysAllowRemoval;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = string.Format("{0} - v.{1}", Assembly.GetExecutingAssembly().GetName().Name, Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            this.PreviewKeyDown += HandleKeyKlick;
+
+            AlwaysAllowRemoval = AllowForceDelete();
+
+            if (AlwaysAllowRemoval)
+            {
+                MessageBox.Show("ACHTUNG!!\nWenn sie einen Laufwerksbuchstabe freigeben der gearde genutzt wird, oder Systemkritisch ist, kann das System in einen Instabilen Zustand versetzt werden!", "Vorsicht", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+
+            btnFreeUpSelected.IsEnabled = false;
+        }
+
+        private void HandleKeyKlick(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                //Beenden wenn Esc gedrückt wurde
+                Close();
+            }
+        }
+
+        /// <summary>
+        /// Prüft ob über den Startparameter angegeben wurde, ob jeder Laufwerksbuchstabe freigegeben werden kann
+        /// </summary>
+        /// <returns></returns>
+        private bool AllowForceDelete()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length <= 1)
+            {
+                return false;
+            }
+
+            try
+            {
+                return args[1].ToLower() == "f";
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SystemDriveLetters = DriveLetter.GetReservedFromRegistry(AlwaysAllowRemoval).ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Es ist ein Fehler beim Laden der Laufwerksbuchstaben aufgetreten\n\n" + ex.Message, "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+                Close();
+            }
+
+            DriveLetterList.ItemsSource = SystemDriveLetters;
+        }
+
+        private void btnFreeUpSelected_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Sollen die ausgewählten Laufwerksbuchstaben freigegeben werden?", "Fortfahren", MessageBoxButton.YesNo, MessageBoxImage.Question) != MessageBoxResult.Yes)
+            {
+                //Auswahl soll doch nicht entfernt werden
+                return;
+            }
+
+            string FailedLetter = "";
+
+            try
+            {
+                int counter = 0;
+                foreach (DriveLetter letter in SystemDriveLetters)
+                {
+                    if (letter.MarkedForRemoval)
+                    {
+                        FailedLetter = letter.Letter.ToString();
+                        letter.Delete();
+                        counter++;
+                    }
+                }
+
+                MessageBox.Show($"Die gewählten {counter} Laufwerksbuchstaben wurden freigegeben", "Erfolgreich", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Beim freigeben des Laufwerkbuchstabends {FailedLetter}, ist ein Fehler aufgetreten.\n\n" + ex.Message, "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            Close();
+        }
+
+        /// <summary>
+        /// Ist mindestens ein Laufwerksbuchstabe zum entfernen ausgewählt?
+        /// </summary>
+        /// <returns></returns>
+        private bool AtLeastOneValidChecked()
+        {
+            foreach (var item in DriveLetterList.Items)
+            {
+                if (item is DriveLetter)
+                {
+                    if ((item as DriveLetter).MarkedForRemoval)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private void ToRemoveSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            btnFreeUpSelected.IsEnabled = AtLeastOneValidChecked();
+        }
+    }
+}
